8d5c85109565bc70ec8b18e15aa357b4dfdc7704
angelina <butz.angelina@yandex.ru>
2026-05-16 21:52:05 +0300

    add: base models for chat

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
+# когда неотдавалась админка помогло python manage.py collectstatic
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+] 
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- backend/chat/models.py
+++ backend/chat/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Room(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    bumped_at = models.DateTimeField(auto_now_add=True)
+    last_message = models.ForeignKey(
+        'Message', related_name='last_message_rooms',
+        on_delete=models.SET_NULL, null=True, blank=True,
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class RoomMember(models.Model):
+    room = models.ForeignKey(Room, related_name='memberships', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name='rooms', on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('room', 'user')
+
+    def __str__(self):
+        return f"{self.user.username} in {self.room.name}"
+
+
+class Message(models.Model):
+    room = models.ForeignKey(Room, related_name='messages', on_delete=models.CASCADE)
+    # message may have null user – we consider such messages "system"
+    user = models.ForeignKey(
+        User, related_name='messages', on_delete=models.CASCADE, null=True)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
