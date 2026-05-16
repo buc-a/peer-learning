c6396ce79db343844a11512df7d1e5263b5ca914
angelina <butz.angelina@yandex.ru>
2026-05-16 21:09:48 +0300

    add: postges + docker-compose

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'grandchat',
+        'USER': 'grandchat',
+        'PASSWORD': 'grandchat',
+        'HOST': 'db', # TODO: заменить 
+        'PORT': '5432',
     }
 }
 
